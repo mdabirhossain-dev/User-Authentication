e4e455477fd8f158496dd7d2b10a2b9f793c6769
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-02-28 13:43:21 +0600

    Login button design, outlet and actions added for Facebook, Google and Apple.

--- User Authentication/ViewController.swift
+++ User Authentication/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var facebookLoginOutlet: UIButton!
+    @IBOutlet weak var googleLoginOutlet: UIButton!
+    @IBOutlet weak var appleLoginOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didTappedFacebookLogin(_ sender: UIButton) {
+        print("didTappedFacebookLogin")
+    }
+    
+    @IBAction func didTappedGoogleLogin(_ sender: UIButton) {
+        print("didTappedGoogleLogin")
+    }
+    
+    @IBAction func didTappedAppleLogin(_ sender: UIButton) {
+        print("didTappedAppleLogin")
+    }
 }
 
